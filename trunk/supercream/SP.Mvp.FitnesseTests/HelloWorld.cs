17e6229101ce3fa79932b56ac22b02e794a3e3bf
coxhead.john <coxhead.john@e626e570-1d99-11df-a1b3-3914820868db>
2010-10-09 00:26:02 +0000

    Change of Namespace for Hello WOrld fitnesse test

--- trunk/supercream/SP.Mvp.FitnesseTests/HelloWorld.cs
+++ trunk/supercream/SP.Mvp.FitnesseTests/HelloWorld.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace HRMossFitnesseTest
+﻿namespace SP.Mvp.FitnesseTests
 {
     public class OurFirstTest : fit.ColumnFixture
     {
