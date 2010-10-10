4dc2ca47ee6dae98ba4aa53ad317c48ff425c22c
coxhead.john <coxhead.john@e626e570-1d99-11df-a1b3-3914820868db>
2010-10-10 17:47:06 +0000

    Added Fitnesse handling and start of Repository Tests

--- trunk/supercream/SP.Mvp.ConsoleTest/Program.cs
+++ trunk/supercream/SP.Mvp.ConsoleTest/Program.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using SP.Mvp.FitnesseTests;
+
 namespace SP.Mvp.ConsoleTest
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var offerDoFixture = new OfferDoFixture();
+            offerDoFixture.SaveOffer();
         }
     }
 }
