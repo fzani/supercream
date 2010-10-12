61e81c3ffb9b326ec2942dff03e033f16399bf6a
coxhead.john <coxhead.john@e626e570-1d99-11df-a1b3-3914820868db>
2010-10-12 22:44:24 +0000

    Changes to styling and addition of Offer Items

---
+++ trunk/supercream/SuperCreamWS/Controls/MaintainOfferItems.ascx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Controls_MaintainOfferItems : System.Web.UI.UserControl
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void ListView1_ItemDataBound(object sender, ListViewItemEventArgs e)
+    {
+        if (e.Item.ItemType == ListViewItemType.DataItem)
+        {
+            var productDescriptionLabel = e.Item.FindControl("ProductDescriptionLabel") as Label;
+            if (productDescriptionLabel != null)
+            {
+                var productUi = new ProductUI();
+
+                var offerItem =
+                    (((ListViewDataItem)e.Item).DataItem) as WcfFoundationService.OfferItem;
+
+                if (offerItem != null)
+                {
+                    var product = productUi.GetProductByID(offerItem.ProductId);
+                    productDescriptionLabel.Text = product.Description;
+                }
+            }
+        }
+    }
+}
